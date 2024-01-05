ee58337799a2583e3d311c4644036d6c349b9536
AntonyShaps <16188347+AntonyShaps@users.noreply.github.com>
2024-01-05 19:31:58 +0100

    3.15, 3.16, 3.18

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+app.use(express.static('dist'))
 app.use(express.json())
 morgan.token('body', (request) => JSON.stringify(request.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 const cors = require('cors')
 const Person = require('./models/person')
 app.use(cors())
-app.use(express.static('dist'))
+
 
 
 app.get('/', (request, response) => {
@@ -21,26 +22,35 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  Person.findById(request.params.id).then(persos=>{
-    response.json(note)
-  })
+app.get('/api/persons/:id', (request, response,next) => {
+  Person.findById(request.params.id).then(person=>{
+    if(person) {
+      response.json(person)
+    }else{
+      response.status(404).end()
+    }
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/info', (request, response) => {
-    const now = new Date()
-    const infoString = `<p>Phonebook has info for ${persons.length} people</p>
-                        <p>${now}</p>`
-    response.send(infoString)
+app.get('/api/info', (request, response,error) => {
+  const now = new Date()
+
+  Person.countDocuments().then(count => {
+      const infoString = `<p>Phonebook has info for ${count} people</p>
+                          <p>${now}</p>`
+      response.send(infoString)
+  }).catch(error => next(error))
 })
+
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-
-  response.status(204).end()
+  Person.findByIdAndDelete(request.params.id)
+  .then(result=>{
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
-
 
 
 app.post('/api/persons', (request, response) => {
@@ -55,6 +65,18 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
