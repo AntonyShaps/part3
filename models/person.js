96f67a8b0b9b421363ba747c5317264411cbab87
AntonyShaps <16188347+AntonyShaps@users.noreply.github.com>
2024-01-06 15:16:32 +0100

    3.22

--- models/person.js
+++ models/person.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose')
 
-mongoose.set('strictQuery',false)
+mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 console.log('conntecting to', url)
 
-
 mongoose.connect(url)
-  .then(result => {
+  .then(() => {
     console.log('connected to MongoDB')
   })
   .catch((error) => {
@@ -18,28 +17,25 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     minLength: 3,
-    required: true
+    required: true,
   },
   number: {
     type: String,
     validate: {
-      validator: function(v) {
+      validator(v) {
         return /^(?:\d{2,3})-(?:\d+)$/.test(v) && v.length >= 8
       },
-      message: props => `${props.value} is not a valid phone number!`
+      message: (props) => `${props.value} is not a valid phone number!`,
     },
-    required: [true, 'User phone number required']
-  }
+    required: [true, 'User phone number required'],
+  },
 })
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })
-
-
-
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
 module.exports = mongoose.model('Person', personSchema)
